0efda57167df880f67437f85cb509ece8053bb92
Alan Chen <mvpi3.tw@gmail.com>
2016-08-11 15:12:33 +0800

    add localized strings file

---
+++ TZImagePickerController/TZImagePickerController/TZLocalization.h
@@ -0,0 +1,48 @@
+//
+//  TZLocalization.h
+//  TZImagePickerController
+//
+//  Created by alan on 2016/8/11.
+//  Copyright © 2016年 谭真. All rights reserved.
+
+#define TZPHOTO_ROLL        NSLocalizedString(@"TZPHOTO_ROLL", @"相機膠卷")
+#define TZPHOTO_STREAM      NSLocalizedString(@"TZPHOTO_STREAM", @"我的照片流")
+#define TZPHOTO_ADDED       NSLocalizedString(@"TZPHOTO_ADDED", @"最近加入")
+#define TZPHOTO_SELFIES     NSLocalizedString(@"TZPHOTO_SELFIES", @"自拍")
+#define TZPHOTO_SHOTS       NSLocalizedString(@"TZPHOTO_SHOTS", @"螢幕截圖")
+#define TZPHOTO_VIDEO       NSLocalizedString(@"TZPHOTO_VIDEO", @"影片")
+#define TZPHOTO_PANORAMAS   NSLocalizedString(@"TZPHOTO_PANORAMAS", @"全景照片")
+#define TZPHOTO_FAVORITES   NSLocalizedString(@"TZPHOTO_FAVORITES", @"個人收藏")
+#define TZPHOTO_ALLPHOTOS   NSLocalizedString(@"TZPHOTO_ALLPHOTOS", @"所有照片")
+
+#define TZPHOTO_BACK        NSLocalizedString(@"TZPHOTO_BACK", @"返回")
+#define TZPHOTO_CONFIRM     NSLocalizedString(@"TZPHOTO_CONFIRM", @"確定")
+#define TZPHOTO_CANCEL      NSLocalizedString(@"TZPHOTO_CANCEL", @"取消")
+#define TZPHOTO_GOTIT       NSLocalizedString(@"TZPHOTO_GOTIT", @"我知道了")
+#define TZPHOTO_PREVIEW     NSLocalizedString(@"TZPHOTO_PREVIEW", @"預覽")
+#define TZPHOTO_SETTINGS    NSLocalizedString(@"TZPHOTO_SETTINGS", @"設定")
+#define TZPHOTO_RAW_PHOTO   NSLocalizedString(@"TZPHOTO_RAW_PHOTO", @"原圖")
+#define TZPHOTO_HANDLING    NSLocalizedString(@"TZPHOTO_HANDLING", @"正在處理...")
+
+#define TZPHOTO_ASK_ALBUM   NSLocalizedString(@"TZPHOTO_ASK_ALBUM", @"請在%1$@的\"設定-隱私權-照片\"選項中，\r允許%2$@訪問您的手機相簿。")
+#define TZPHOTO_ASK_CAMERA  NSLocalizedString(@"TZPHOTO_ASK_CAMERA", @"請在%1$@的\"設定-隱私權-相繼\"選項中，\r允許%2$@使用您的相機。")
+#define TZPHOTO_MAX_PHOTOS  NSLocalizedString(@"TZPHOTO_MAX_PHOTOS", @"您最多只選擇%1$ld張照片")
+
+#define TZPHOTO_PROMPT_1    NSLocalizedString(@"TZPHOTO_PROMPT_1", @"選擇照片時不能選擇影片")
+#define TZPHOTO_PROMPT_2    NSLocalizedString(@"TZPHOTO_PROMPT_2", @"無法使用相機")
+#define TZPHOTO_PROMPT_3    NSLocalizedString(@"TZPHOTO_PROMPT_3", @"多選狀態下選擇影片，預設將影片當圖片發送")
+
+#define TZPHOTO_TITLE_PREVIEW   NSLocalizedString(@"TZPHOTO_TITLE_PREVIEW", @"影片預覽")
+#define TZPHOTO_TITLE_PHOTO     NSLocalizedString(@"TZPHOTO_TITLE_PHOTO", @"照片")
+
+
+
+
+
+
+
+
+
+
+
+
